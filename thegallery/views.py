25ff579e9660295d647821e0768b908bbdf82f7e
Anum Asif <anum@cockar.com>
2019-03-03 13:24:35 +0300

    Add view function to display images on the index page

--- thegallery/views.py
+++ thegallery/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.http import HttpResponse, Http404
+from django.shortcuts import render, redirect
+from .models import Image, categories
 
 # Create your views here.
+def gallery(request):
+    images = Image.get_images
+    img_categories = categories.get_categories
+    return render(request, 'index.html', {"images":images, "categories":img_categories})
