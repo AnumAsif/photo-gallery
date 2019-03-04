c39867f0833b43867b19e1a2d7f54f331a23f4df
Anum Asif <anum@cockar.com>
2019-03-04 11:20:47 +0300

    Add tests for the image class

--- thegallery/tests.py
+++ thegallery/tests.py
@@ -1,3 +1,26 @@
-from django.test import TestCase
+# from django.test import TestCase
+# from .models import Editor,Article,tags
 
-# Create your tests here.
+# # Create your tests here.
+# class ImagesTestClass(TestCase):
+
+#     # Set up method
+#     def setUp(self):
+#         self.newImg= Image(name = 'waterfall', description ='taken at karura forest', location ='Nairobi', categories)
+
+#     # Testing  instance
+#     def test_instance(self):
+#         self.assertTrue(isinstance(self.james,Editor))
+
+#       # Testing Save Method
+#     def test_save_method(self):
+#         self.james.save_editor()
+#         editors = Editor.objects.all()
+#         self.assertTrue(len(editors) > 0)    
+
+
+#     #       name = models.CharField(max_length=50)
+#     # description = models.TextField()
+#     # img= models.ImageField(height_field=None, width_field=None, max_length=None)
+#     # location = models.ForeignKey(Location, on_delete=models.CASCADE)
+#     # categories = models.ManyToManyField(categories)
